a4c70e41e5ed8c165f6ba5a3d0211c366134c7b0
MuhammadIsmailYousuf <ismailyousuf192005@gmail.com>
2024-07-12 11:41:30 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+//arrow function use =>;
+//EXAMPLE 1;
+const x = (x, y) => x * y;
+document.getElementById("demo").innerHTML = x(5, 5);
+
+//EXAMPLE 2;
+let y = (a, b) => a + b;
+document.getElementById('main').innerHTML = y(7, 5);
+
+//EXAMPLE 3;
+var z = (p, q) => p - q;
+document.getElementById('script').innerHTML = z(5, 5);
+
+//EXAMPLE 4;
+var c = (x , y , z) => x / y / z;
+document.getElementById('app').innerHTML = c(2,5,6);
